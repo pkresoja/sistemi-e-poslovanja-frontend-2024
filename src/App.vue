5bee56d5ff7f0c99ebdcf5dd88f84f21655d3ae1
Petar Kresoja <pkresoja@singidunum.ac.rs>
2024-05-15 17:08:22 +0200

    Add Fonts Awesome and basic Navigation

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,31 @@
 </script>
 
 <template>
-  <h1>Hello World from Sistemi e poslovanja</h1>
-  <RouterLink to="/">HOME</RouterLink> | <RouterLink to="/type">TYPE</RouterLink>
-  <RouterView />
+  <div class="container">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <RouterLink class="navbar-brand" to="/">
+          <i class="fa-solid fa-sheet-plastic"></i> PSEP
+        </RouterLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/type">Types</RouterLink>
+            </li>
+          </ul>
+          <span class="navbar-text">
+            Korisnik 123
+          </span>
+        </div>
+      </div>
+    </nav>
+    <RouterView />
+  </div>
 </template>
